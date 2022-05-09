63600259cd89e8731dee459b37644970c26f1dbc
Ali Mansour <dev.ali.mansour@gmail.com>
2022-05-09 21:22:18 +0200

    feat: Design Home Screen Top Bar.
    Create HomeTopBar.kt and create HomeTopBar() composable function with onSearchClicked function parameter to act as home top bar and use it in HomeScreen as Scaffold's topBar.

--- app/src/main/java/dev/alimansour/borutoapp/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/dev/alimansour/borutoapp/presentation/screen/home/HomeScreen.kt
@@ -1,9 +1,18 @@
 package dev.alimansour.borutoapp.presentation.screen.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            HomeTopBar {
 
+            }
+        }
+    ) {
+
+    }
 }
