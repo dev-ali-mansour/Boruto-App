37ae07aaa6309c21beaeb0c5dcafffef4b217950
Ali Mansour <dev.ali.mansour@gmail.com>
2022-05-15 17:48:53 +0200

    refactor: Create FakeBorutoApi For Testing SearchHeroSource.
    Implement BorutoApi interface in FakeBorutoApi and override it's functions for testing.

---
+++ app/src/test/java/dev/alimansour/borutoapp/data/remote/FakeBorutoApi.kt
@@ -0,0 +1,72 @@
+package dev.alimansour.borutoapp.data.remote
+
+import dev.alimansour.borutoapp.data.local.entity.HeroEntity
+import dev.alimansour.borutoapp.data.remote.response.ApiResponse
+
+class FakeBorutoApi : BorutoApi {
+
+    private val heroes = listOf(
+        HeroEntity(
+            id = 1,
+            name = "Sasuke",
+            image = "",
+            about = "",
+            rating = 5.0,
+            power = 0,
+            month = "",
+            day = "",
+            family = listOf(),
+            abilities = listOf(),
+            natureTypes = listOf()
+        ),
+        HeroEntity(
+            id = 2,
+            name = "Naruto",
+            image = "",
+            about = "",
+            rating = 5.0,
+            power = 0,
+            month = "",
+            day = "",
+            family = listOf(),
+            abilities = listOf(),
+            natureTypes = listOf()
+        ),
+        HeroEntity(
+            id = 3,
+            name = "Sakura",
+            image = "",
+            about = "",
+            rating = 5.0,
+            power = 0,
+            month = "",
+            day = "",
+            family = listOf(),
+            abilities = listOf(),
+            natureTypes = listOf()
+        )
+    )
+
+    override suspend fun getAllHeroes(page: Int, limit: Int): ApiResponse =
+        ApiResponse(success = false)
+
+    override suspend fun searchHeroes(name: String): ApiResponse =
+        ApiResponse(
+            success = true,
+            message = "Ok",
+            heroes = findHeroes(name = name),
+        )
+
+    private fun findHeroes(name: String): List<HeroEntity> {
+        val founded = mutableListOf<HeroEntity>()
+        return when {
+            name.isNotEmpty() -> {
+                heroes.forEach { hero ->
+                    founded.add(hero)
+                }
+                founded
+            }
+            else -> emptyList()
+        }
+    }
+}
